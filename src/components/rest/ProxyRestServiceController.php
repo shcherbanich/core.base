7342c4e79e82fb35defe4a459208a2b7f896911f
Philip <filippF@bk.ru>
2018-03-04 16:14:01 +0300

    Основные изменения

---
+++ src/components/rest/ProxyRestServiceController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace shcherbanich\core\components\rest;
+
+use Yii;
+use yii\base\InvalidParamException;
+
+class ProxyRestServiceController extends \yii\base\Controller
+{
+
+    public $controllerName = '';
+
+    public $serviceName = '';
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+
+        if(!$this->controllerName){
+
+            throw new InvalidParamException("controllerName must be set");
+        }
+
+        if(!$this->serviceName){
+
+            throw new InvalidParamException("serviceName must be set");
+        }
+
+        Yii::$app->user->enableSession = false;
+
+        $headers = Yii::$app->response->headers;
+
+        $headers->add('Access-Control-Allow-Origin', '*');
+
+        $headers->add('Access-Control-Allow-Credentials', 'true');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function runAction($id, $params = []){
+
+        $serviceRequest = new \shcherbanich\core\helpers\microService\Request;
+
+        $sendParams = [];
+
+        foreach ($params as $key => $param){
+
+            $sendParams[] = "{$key}={$param}";
+        }
+
+        $serviceRequest->setCommand("{$this->controllerName}/{$id}?".implode('&', $sendParams));
+
+        Yii::$app->{$this->serviceName}->addRequestHandler('all', function($request){
+
+            foreach(\Yii::$app->request->headers as $name => $header){
+
+                if(!in_array($name, ['host', 'content-length', 'connection', 'accept-encoding', 'accept'])) {
+
+                    $request->addHeaders([$name => $header]);
+                }
+            }
+        });
+
+        $sendParams = Yii::$app->getRequest()->getBodyParams();
+
+        $sendParams = is_array($sendParams) ? $sendParams : [];
+
+        $serviceRequest->setParams($sendParams);
+
+        return Yii::$app->{$this->serviceName}->sendRequest($serviceRequest, ['method' => Yii::$app->request->getMethod()]);
+    }
+}
