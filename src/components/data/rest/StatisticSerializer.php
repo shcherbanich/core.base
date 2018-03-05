d5f4c6c09dd720002fbf39dee29e31cee4624a35
Philip <filippF@bk.ru>
2018-03-05 12:34:15 +0300

    Основные изменения

--- src/components/data/rest/StatisticSerializer.php
+++ src/components/data/rest/StatisticSerializer.php
@@ -2,8 +2,35 @@
 
 namespace shcherbanich\core\components\data\rest;
 
+use yii\base\Arrayable;
+use yii\base\Model;
+use yii\data\DataProviderInterface;
+
 class StatisticSerializer extends \yii\rest\Serializer
 {
+    public $className;
+
+    /**
+     * Serializes the given data into a format that can be easily turned into other formats.
+     * This method mainly converts the objects of recognized types into array representation.
+     * It will not do conversion for unknown object types or non-object data.
+     * The default implementation will handle [[Model]] and [[DataProviderInterface]].
+     * You may override this method to support more object types.
+     * @param mixed $data the data to be serialized.
+     * @return mixed the converted data.
+     */
+    public function serialize($data, $className = '')
+    {
+        if ($data instanceof Model && $data->hasErrors()) {
+            return $this->serializeModelErrors($data);
+        } elseif ($data instanceof Arrayable) {
+            return $this->serializeModel($data);
+        } elseif ($data instanceof DataProviderInterface) {
+            return $this->serializeDataProvider($data);
+        }
+
+        return $data;
+    }
 
     /**
      * Serializes a set of models.
@@ -16,9 +43,16 @@ class StatisticSerializer extends \yii\rest\Serializer
 
         $callbackExpands = [];
 
-        $model = current($models);
+        if($this->className) {
 
-        $extraFields = $model->extraFields();
+            $model = new $this->className;
+
+            $extraFields = $model->extraFields();
+        }
+        else{
+
+            $extraFields = [];
+        }
 
         foreach ($expand as $k => $extraField) {
 
